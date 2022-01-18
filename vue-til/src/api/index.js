f0991202b9574b26204856449b903e71e2f12e0a
wonsikgo <45756634+wonsikgo@users.noreply.github.com>
2022-01-18 23:44:37 +0900

    LoginForm 생성, api연결

--- vue-til/src/api/index.js
+++ vue-til/src/api/index.js
@@ -2,11 +2,14 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL: process.env.VUE_APP_API_URL,
-  //baseURL: 'http://localhost:3000/',
 });
 
 function registerUser(userData) {
   return instance.post('signup', userData);
 }
 
-export { registerUser };
+function loginUser(userData) {
+  return instance.post('login', userData);
+}
+
+export { registerUser, loginUser };
---
+++ vue-til/src/components/LoginForm.vue
@@ -0,0 +1,47 @@
+<template>
+  <form @submit.prevent="submitForm">
+    <div>
+      <label for="username">id:</label>
+      <input id="username " type="text" v-model="username" />
+    </div>
+    <div>
+      <label for="password">pw:</label>
+      <input id="password " type="password" v-model="password" />
+    </div>
+    <button>login</button>
+    <p>{{ logMessage }}</p>
+  </form>
+</template>
+
+<script>
+import { loginUser } from '@/api/index';
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+      logMessage: '',
+    };
+  },
+  methods: {
+    async submitForm() {
+      const userData = {
+        username: this.username,
+        password: this.password,
+      };
+
+      const { data } = await loginUser(userData);
+
+      console.log(data);
+    },
+
+    initForm() {
+      this.username = '';
+      this.password = '';
+      this.logMessage = '';
+    },
+  },
+};
+</script>
+
+<style></style>
