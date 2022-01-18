eda82f856f65511552e2eb1898c4adffe99aff1b
wonsikgo <45756634+wonsikgo@users.noreply.github.com>
2022-01-19 00:51:50 +0900

    Vuex 추가, 로그아웃추가

--- vue-til/src/components/LoginForm.vue
+++ vue-til/src/components/LoginForm.vue
@@ -54,7 +54,9 @@ export default {
         };
 
         const { data } = await loginUser(userData);
-        this.logMessage = `${data.user.username} 님 환영합니다.`;
+
+        this.$store.commit('setUsername', data.user.username);
+        this.$router.push('/main');
       } catch (error) {
         this.logMessage = error.response.data;
       } finally {
