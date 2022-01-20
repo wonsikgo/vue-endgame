b1c9727c2b4450d65ff178358ea437706ca6d369
wonsikgo <45756634+wonsikgo@users.noreply.github.com>
2022-01-20 22:49:13 +0900

    TOKEN 관리

--- vue-til/src/components/common/AppHeader.vue
+++ vue-til/src/components/common/AppHeader.vue
@@ -1,11 +1,13 @@
 <template>
   <header>
     <div>
-      <router-link to="/" class="logo"> TIL </router-link>
+      <router-link to="/" class="logo">
+        TIL
+        <span v-if="isUserLogin">by {{ $store.state.username }}</span>
+      </router-link>
     </div>
     <div class="navigations">
       <template v-if="isUserLogin">
-        <span>{{ $store.state.username }}</span>
         <a href="javascript:;" @click="logoutUser">Logout</a>
       </template>
       <template v-else>
