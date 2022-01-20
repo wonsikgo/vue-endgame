256d3a12b34f7778e8ee76ecd568065aaef9f2c7
wonsikgo <45756634+wonsikgo@users.noreply.github.com>
2022-01-20 23:23:46 +0900

    로딩 스피너 추가

--- vue-til/src/views/MainPage.vue
+++ vue-til/src/views/MainPage.vue
@@ -1,28 +1,31 @@
 <template>
   <div class="main list-container contents">
     <h1 class="page-header">Today I Learned</h1>
-    <ul>
-      <li v-for="postItem in postItems" :key="postItem._id">
-        <div class="post-title">
-          {{ postItem.title }}
-        </div>
-        <div class="post-contents">
-          {{ postItem.contents }}
-        </div>
-        <div class="post-time">
-          {{ postItem.createdAt }}
-        </div>
-      </li>
+    <LoadingSpinner v-if="isLoading" />
+    <ul v-else>
+      <PostListItem
+        v-for="postItem in postItems"
+        :key="postItem._id"
+        :postItem="postItem"
+      >
+      </PostListItem>
     </ul>
   </div>
 </template>
 
 <script>
+import PostListItem from '@/components/posts/PostListItem.vue';
+import LoadingSpinner from '@/components/common/LoadingSpinner.vue';
 import { fetchPosts } from '@/api/index';
 export default {
+  components: {
+    PostListItem,
+    LoadingSpinner,
+  },
   data() {
     return {
       postItems: [],
+      isLoading: false,
     };
   },
   methods: {
