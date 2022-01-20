07487df79c18b4db714bc3c65a294a235b454c2c
wonsikgo <45756634+wonsikgo@users.noreply.github.com>
2022-01-21 01:50:28 +0900

    게시판 버튼 추가

--- vue-til/src/views/MainPage.vue
+++ vue-til/src/views/MainPage.vue
@@ -1,15 +1,20 @@
 <template>
-  <div class="main list-container contents">
-    <h1 class="page-header">Today I Learned</h1>
-    <LoadingSpinner v-if="isLoading" />
-    <ul v-else>
-      <PostListItem
-        v-for="postItem in postItems"
-        :key="postItem._id"
-        :postItem="postItem"
-      >
-      </PostListItem>
-    </ul>
+  <div>
+    <div class="main list-container contents">
+      <h1 class="page-header">Today I Learned</h1>
+      <LoadingSpinner v-if="isLoading" />
+      <ul v-else>
+        <PostListItem
+          v-for="postItem in postItems"
+          :key="postItem._id"
+          :postItem="postItem"
+        >
+        </PostListItem>
+      </ul>
+    </div>
+    <router-link to="/add" class="create-button">
+      <i class="ion-md-add"></i>
+    </router-link>
   </div>
 </template>
 
